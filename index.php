32f4054e9f51e6e100063986c8a6da62dba2457c
Sudarshan Chaudhari <30308970+sudarshan-uc@users.noreply.github.com>
2017-10-27 10:51:17 +0000

    Update index.php

--- index.php
+++ index.php
@@ -61,11 +61,9 @@
             <li><a href="#">Home</a></li>
             <li>Basic</li>
           </ol>
-          <h1>Welcome to an OpenShift Application! Created by Mr. Jatan Malde</h1>
+          <h1>Welcome to an OpenShift Application! Created by Mr. Sudarshan Chaudhari</h1>
           <p>The purpose of this application is to demonstrate several interesting features about OpenShift. We hope you enjoy it!
-          <h2>For more about the creator click on the link below</h2><a href="https://jatanmalde.wordpress.com/">here</a><br>
-          For more about the creator click on the link below<a href="https://jatanmalde.github.io/">here</a></p>
-        </div>
+          </div>
       </div>
       <div class="row">
         <div class="col-md-6 col-md-offset-3">
